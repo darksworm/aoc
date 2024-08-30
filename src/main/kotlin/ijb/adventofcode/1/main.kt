f402b05a76cc547efda9ba17046c16e07fc2d037
darksworm <9987548+darksworm@users.noreply.github.com>
2024-08-30 19:10:25 +0200

    implemented #2

--- src/main/kotlin/ijb/adventofcode/1/main.kt
+++ src/main/kotlin/ijb/adventofcode/1/main.kt
@@ -3,27 +3,78 @@ package ijb.adventofcode.`1`
 import java.io.File
 
 fun main() {
-    val file = "/Users/pPersonal/Dev/ijb.adventofcode/src/main/kotlin/ijb/adventofcode/1/input.txt"
+    val mapping = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9"
+    )
 
+    val file = "/Users/pPersonal/Dev/ijb.adventofcode/src/main/kotlin/ijb/adventofcode/1/input.txt"
     var totalNumber = 0;
 
     File(file).forEachLine { line ->
-        var number: String  = ""
-        var lastNumberDigit: String = ""
+        var earliestPosition: Int = line.length + 99
+        var earliestDigit: String = ""
 
-        line.chars().forEach{ character ->
-            if (!Character.isDigit(character)) {
-                return@forEach
+        var lastPosition: Int = 0
+        var lastDigit: String = ""
+
+        for((text, numberDigit) in mapping) {
+            val textPos = line.indexOf(text)
+            val digitPos = line.indexOf(numberDigit)
+
+            if (textPos != -1) {
+                val textPosLast = line.lastIndexOf(text)
+
+                if (earliestPosition > textPos) {
+                    earliestDigit = numberDigit
+                    earliestPosition = textPos
+                }
+
+                if (lastPosition < textPos) {
+                    lastDigit = numberDigit
+                    lastPosition = textPos
+                }
+
+                if (lastPosition < textPosLast) {
+                    lastDigit = numberDigit
+                    lastPosition = textPosLast
+                }
             }
 
-            lastNumberDigit = character.toChar().toString()
+            if (digitPos != -1) {
+                val digitPosLast = line.lastIndexOf(numberDigit)
 
-            if (number.isEmpty()) {
-                number = lastNumberDigit
+                if (earliestPosition > digitPos) {
+                    earliestDigit = numberDigit
+                    earliestPosition = digitPos
+                }
+
+                if (lastPosition < digitPos) {
+                    lastDigit = numberDigit
+                    lastPosition = digitPos
+                }
+
+                if (lastPosition < digitPosLast) {
+                    lastDigit = numberDigit
+                    lastPosition = digitPosLast
+                }
             }
         }
 
-        totalNumber += "${number}${lastNumberDigit}".toInt()
+        if (lastDigit.isEmpty()) {
+            lastDigit = earliestDigit
+        }
+
+        println("line ${line} firstDigit ${earliestDigit} lastDigit ${lastDigit}")
+
+        totalNumber += "${earliestDigit}${lastDigit}".toInt()
     }
 
     println("your result: ${totalNumber}")

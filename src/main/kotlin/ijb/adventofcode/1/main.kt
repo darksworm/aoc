d97bd3a7cfe3b32dd210bf1f2114716b159c2669
Ilmārs <132436248+ilmarspenneo@users.noreply.github.com>
2024-12-01 10:11:17 +0100

    implemented 2024.#1

--- src/main/kotlin/ijb/adventofcode/1/main.kt
+++ src/main/kotlin/ijb/adventofcode/1/main.kt
@@ -1,81 +1,41 @@
 package ijb.adventofcode.`1`
 
 import java.io.File
+import kotlin.math.abs
 
-fun main() {
-    val mapping = mapOf(
-        "one" to "1",
-        "two" to "2",
-        "three" to "3",
-        "four" to "4",
-        "five" to "5",
-        "six" to "6",
-        "seven" to "7",
-        "eight" to "8",
-        "nine" to "9"
-    )
+fun read(filePath: String): Pair<List<Int>, List<Int>> {
+    val leftList = mutableListOf<Int>();
+    val rightList = mutableListOf<Int>();
 
-    val file = "/Users/pPersonal/Dev/ijb.adventofcode/src/main/kotlin/ijb/adventofcode/1/input.txt"
-    var totalNumber = 0;
-
-    File(file).forEachLine { line ->
-        var earliestPosition: Int = line.length + 99
-        var earliestDigit: String = ""
-
-        var lastPosition: Int = 0
-        var lastDigit: String = ""
-
-        for((text, numberDigit) in mapping) {
-            val textPos = line.indexOf(text)
-            val digitPos = line.indexOf(numberDigit)
-
-            if (textPos != -1) {
-                val textPosLast = line.lastIndexOf(text)
-
-                if (earliestPosition > textPos) {
-                    earliestDigit = numberDigit
-                    earliestPosition = textPos
-                }
-
-                if (lastPosition < textPos) {
-                    lastDigit = numberDigit
-                    lastPosition = textPos
-                }
-
-                if (lastPosition < textPosLast) {
-                    lastDigit = numberDigit
-                    lastPosition = textPosLast
-                }
-            }
-
-            if (digitPos != -1) {
-                val digitPosLast = line.lastIndexOf(numberDigit)
-
-                if (earliestPosition > digitPos) {
-                    earliestDigit = numberDigit
-                    earliestPosition = digitPos
-                }
-
-                if (lastPosition < digitPos) {
-                    lastDigit = numberDigit
-                    lastPosition = digitPos
-                }
-
-                if (lastPosition < digitPosLast) {
-                    lastDigit = numberDigit
-                    lastPosition = digitPosLast
-                }
-            }
-        }
-
-        if (lastDigit.isEmpty()) {
-            lastDigit = earliestDigit
-        }
-
-        println("line ${line} firstDigit ${earliestDigit} lastDigit ${lastDigit}")
-
-        totalNumber += "${earliestDigit}${lastDigit}".toInt()
+    File(filePath).forEachLine { line ->
+        val (l, r) = line.split("   ")
+        leftList += l.toInt()
+        rightList += r.toInt()
     }
 
-    println("your result: ${totalNumber}")
+    return Pair(leftList, rightList)
+}
+
+fun sort(input: Pair<List<Int>, List<Int>>): Pair<List<Int>, List<Int>> {
+    return Pair(
+        input.first.sorted(),
+        input.second.sorted(),
+    )
+}
+
+fun main() {
+    val file = "/Users/ilmars/Dev/ijb.adventofcode/src/main/kotlin/ijb/adventofcode/1/input.txt"
+
+    val input = read(file)
+    val sorted = sort(input)
+
+    assert(sorted.first.size == sorted.second.size)
+
+    val differences = sorted.first.mapIndexed { index, firstVal ->
+        abs(firstVal - sorted.second[index])
+    }
+
+    val sum = differences.sum()
+
+    println("Your result is: $sum")
 }

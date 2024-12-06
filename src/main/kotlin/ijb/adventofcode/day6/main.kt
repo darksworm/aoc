878d3c2ca666c480f890330b9165ac9a23125b0e
Ilmārs <132436248+ilmarspenneo@users.noreply.github.com>
2024-12-06 20:12:04 +0100

    refactor to feel smart 🤓

--- src/main/kotlin/ijb/adventofcode/day6/main.kt
+++ src/main/kotlin/ijb/adventofcode/day6/main.kt
@@ -1,52 +1,47 @@
 package ijb.adventofcode.day6
 
-import kotlinx.coroutines.*
-import java.util.concurrent.atomic.AtomicInteger
-
 import java.io.File
 import java.util.*
-import java.util.concurrent.Executors
 import kotlin.time.measureTime
 
 data class Position(var x: Int, var y: Int)
 
 enum class Direction {
-    L, R, U, D;
+    U, R, D, L
 }
 
-fun read(filePath: String): String {
-    return File(filePath)
+fun read(filePath: String): List<String> =
+    File(filePath)
         .readText(Charsets.UTF_8)
         .trim()
-}
+        .split(System.lineSeparator())
+        .toMutableList()
 
-fun isGoingOOB(lines: List<String>, direction: Direction, position: Position): Boolean =
-     (direction == Direction.U && position.y == 0) ||
-     (direction == Direction.D && position.y == lines.size - 1) ||
-     (direction == Direction.L && position.x == 0) ||
-     (direction == Direction.R && position.x == lines.first().length - 1)
-
-fun nextPosition(direction: Direction, original: Position): Position =
-    when (direction) {
-        Direction.U -> Position(original.x, original.y - 1)
-        Direction.D -> Position(original.x, original.y + 1)
-        Direction.R -> Position(original.x + 1, original.y)
-        Direction.L -> Position(original.x - 1, original.y)
+fun isNextStepOutOfBounds(lines: List<String>, dir: Direction, pos: Position): Boolean =
+    when (dir) {
+        Direction.U -> pos.y == 0
+        Direction.R ->  pos.x == lines.first().length - 1
+        Direction.D ->  pos.y == lines.size - 1
+        Direction.L -> pos.x == 0
     }
 
-fun turnRight(direction: Direction): Direction =
-    when (direction) {
+fun nextPosition(dir: Direction, prev: Position): Position =
+    when (dir) {
+        Direction.U -> Position(prev.x, prev.y - 1)
+        Direction.R -> Position(prev.x + 1, prev.y)
+        Direction.D -> Position(prev.x, prev.y + 1)
+        Direction.L -> Position(prev.x - 1, prev.y)
+    }
+
+fun turnRight(dir: Direction): Direction =
+    when (dir) {
         Direction.U -> Direction.R
         Direction.R -> Direction.D
         Direction.D -> Direction.L
         Direction.L -> Direction.U
     }
 
-fun partOne(input: String): Int {
-    val lines = input
-        .split(System.lineSeparator())
-        .toMutableList()
-
+inline fun walkWhileNotOutOfBounds(lines: List<String>, step: (Position, Direction, Position) -> Unit) {
     val y = lines.indexOfFirst { it.contains("^") }
     assert(y >= 0)
     val x = lines[y].indexOf("^")
@@ -55,131 +50,84 @@ fun partOne(input: String): Int {
     var curPos = Position(x, y)
     var curDir = Direction.U
 
-    val visitedPositions = mutableSetOf(curPos)
-    var turns = 0
-
-    while (!isGoingOOB(lines, curDir, curPos)) {
+    while (!isNextStepOutOfBounds(lines, curDir, curPos)) {
         val nextPos = nextPosition(curDir, curPos)
+
+        step(curPos, curDir, nextPos)
 
         if (lines[nextPos.y][nextPos.x] == '#') {
             curDir = turnRight(curDir)
-            assert(++turns <= 3)
-            continue;
+            continue
         }
-//        // for debugging - show the current position
-//        lines[curPos.y] = lines[curPos.y].toMutableList().apply {
-//            this[curPos.x] = 'X'
-//        }.joinToString("")
 
-//        // for debugging - show the next position
-//        lines[nextPos.y] = lines[nextPos.y].toMutableList().apply {
-//            this[nextPos.x] = '^'
-//        }.joinToString("")
-
-        visitedPositions.add(curPos)
         curPos = nextPos
-        turns = 0
+    }
+}
+
+fun partOne(lines: List<String>): Int {
+    val visitedPositions = mutableSetOf<Position>()
+
+    walkWhileNotOutOfBounds(lines) { curPos, _, _ ->
+        visitedPositions += curPos
     }
 
     // somehow, I'm off by one, 🤷‍♂️
     return visitedPositions.size + 1
 }
 
-fun partTwo(input: String): Int {
-    val lines = input
-        .split(System.lineSeparator())
-        .toMutableList()
+fun calculateAllPossibleWallPlacements(lines: List<String>) = iterator {
+    for ((lineIdx, l) in lines.withIndex()) {
+        for ((letterIdx, _) in l.withIndex()) {
+            if (lines[lineIdx][letterIdx] in setOf('#', '^')) {
+                continue
+            }
 
-    val result = AtomicInteger(0)
-    val dispatcher = Executors.newFixedThreadPool(10).asCoroutineDispatcher()
-
-    System.err.println("Progress (130 total #s):")
-
-    runBlocking(dispatcher) {
-        val jobs = mutableListOf<Deferred<Unit>>()
-
-        for ((lineIdx, line) in lines.withIndex()) {
-            jobs.add(async {
-                for ((letterIdx, _) in line.toList().withIndex()) {
-                    val copy = lines.toMutableList()
-                    if (copy[lineIdx][letterIdx] == '#' || copy[lineIdx][letterIdx] == '^') {
-                        continue
-                    }
-
-                    copy[lineIdx] = lines[lineIdx].toMutableList().apply {
-                        this[letterIdx] = '#'
-                    }.joinToString("")
-
-                    if (hasLoop(copy)) {
-                        result.incrementAndGet()
+            yield(
+                lines.mapIndexed { idx, line ->
+                    if (idx == lineIdx) {
+                        line.toCharArray().apply { this[letterIdx] = '#' }.concatToString()
+                    } else {
+                        line
                     }
                 }
-
-                System.err.print("#")
-            })
+            )
         }
-
-        jobs.awaitAll()
     }
-
-    dispatcher.close()
-    System.err.println()
-
-    return result.get()
 }
 
-fun hasLoop(lines: MutableList<String>): Boolean {
-    val y = lines.indexOfFirst { it.contains("^") }
-    assert(y >= 0)
-    val x = lines[y].indexOf("^")
-    assert(x >= 0)
-
-    var curPos = Position(x, y)
-    var curDir = Direction.U
-
+fun hasLoop(lines: List<String>): Boolean {
     val turnPositions: Stack<Pair<Position, Direction>> = Stack()
-    var cycles = 0
 
-    while (!isGoingOOB(lines, curDir, curPos)) {
-        val nextPos = nextPosition(curDir, curPos)
-
+    walkWhileNotOutOfBounds(lines) { curPos, curDir, nextPos ->
         if (lines[nextPos.y][nextPos.x] == '#') {
-            if(turnPositions.contains(Pair(curPos, curDir))) {
-                return true;
+            if (turnPositions.contains(Pair(curPos, curDir))) {
+                return@hasLoop true
             }
 
             turnPositions.add(Pair(curPos, curDir))
-            curDir = turnRight(curDir)
-
-            continue;
         }
-
-//        // for debugging - show the current position
-//        lines[curPos.y] = lines[curPos.y].toMutableList().apply {
-//            this[curPos.x] = ','
-//        }.joinToString("")
-//
-//        // for debugging - show the next position
-//        lines[nextPos.y] = lines[nextPos.y].toMutableList().apply {
-//            this[nextPos.x] = '^'
-//        }.joinToString("")
-
-        curPos = nextPos
-
-        // crash and burn if we're in a never ending loop
-        assert(++cycles < 1000);
     }
 
-    return false;
+    return false
+}
+
+fun partTwo(lines: List<String>): Int {
+    var result = 0
+
+    calculateAllPossibleWallPlacements(lines)
+        .forEach { result += if (hasLoop(it)) 1 else 0 }
+
+    return result
 }
 
 fun main() {
     val file = "./src/main/kotlin/ijb/adventofcode/day6/input.txt"
+    val content = read(file)
 
-    println("Part one result is: ${partOne(read(file))}")
+    println("Part one result is: ${partOne(content)}")
 
     val timeTaken = measureTime {
-        println("Part two result is: ${partTwo(read(file))}")
+        println("Part two result is: ${partTwo(content)}")
     }
 
     println("Part two took $timeTaken")
